9463242207e6603da82b06cbb30c55a36f60d9a5
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-11-30 17:52:36 -0300

    feat: adição do arquivo MassTransitExtensions.cs

---
+++ developments/AwesomeShop.Services.Orders-dia1-observabilidade/src/AwesomeShop.Services.Orders.API/Extensions/MassTransitExtensions.cs
@@ -0,0 +1,26 @@
+﻿using AwesomeShop.Services.Orders.Application.Subscribers;
+using MassTransit;
+
+namespace AwesomeShop.Services.Orders.API.Extensions;
+public static class MassTransitExtensions
+{
+    public static void AddMassTransitExtension(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMassTransit(x =>
+        {
+            x.AddConsumer<PaymentAcceptedSubscriber>(typeof(PaymentAcceptedSubscriber.PaymentAcceptedSubscriberDefinition));
+
+            x.SetKebabCaseEndpointNameFormatter();
+
+            x.UsingRabbitMq((ctx, cfg) =>
+            {
+                cfg.Host(configuration.GetConnectionString("RabbitMq"));
+
+                cfg.ServiceInstance(instance =>
+                {
+                    instance.ConfigureEndpoints(ctx, new KebabCaseEndpointNameFormatter("dev", false));
+                });
+            });
+        });
+    }
+}
