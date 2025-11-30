c02b4425c021915d0ceb8f9a4924cfeaa8721ed1
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-11-30 17:52:45 -0300

    feat: adição do arquivo ApplicationModule.cs

---
+++ developments/AwesomeShop.Services.Orders-dia1-observabilidade/src/AwesomeShop.Services.Orders.Application/ApplicationModule.cs
@@ -0,0 +1,23 @@
+using AwesomeShop.Services.Orders.Application.Subscribers;
+using AwesomeShop.Services.Orders.Application.UseCases.AddOrder;
+using AwesomeShop.Services.Orders.Application.UseCases.GetOrderById;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AwesomeShop.Services.Orders.Application;
+
+public static class ApplicationModule
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddUseCases();
+        
+        return services;
+    }
+
+    private static IServiceCollection AddUseCases(this IServiceCollection services) {
+        services.AddScoped<IAddOrderUseCase, AddOrderUseCase>();
+        services.AddScoped<IGetOrderByIdUseCase, GetOrderByIdUseCase>();
+            
+        return services;
+    }
+}
