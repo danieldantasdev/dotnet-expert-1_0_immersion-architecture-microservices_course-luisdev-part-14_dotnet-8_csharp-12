8eb4cac302a231bd70d9125963e01f249f29f386
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-11-30 17:52:32 -0300

    feat: adição do arquivo SerilogExtensions.cs

---
+++ developments/AwesomeShop.Services.Orders-dia1-observabilidade/src/AwesomeShop.Services.Orders.Infrastructure/Logging/SerilogExtensions.cs
@@ -0,0 +1,58 @@
+﻿using AwesomeShop.Services.Orders.Infrastructure.Middlewares;
+using Elastic.Apm.SerilogEnricher;
+using Elastic.CommonSchema.Serilog;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Filters;
+using Serilog.Sinks.Elasticsearch;
+
+namespace AwesomeShop.Services.Orders.Infrastructure.Logging;
+public static class SerilogExtensions
+{
+    public static WebApplicationBuilder AddSerilog(this WebApplicationBuilder builder, IConfiguration configuration, string applicationName)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .MinimumLevel.Override("System", LogEventLevel.Information)
+            .MinimumLevel.Override("MassTransit", LogEventLevel.Debug)
+            .Enrich.FromLogContext()
+            .Enrich.WithProperty("ApplicationName", $"{applicationName} - {Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")}")
+            .Enrich.WithElasticApmCorrelationInfo()
+            .Enrich.WithCorrelationId()
+            .Enrich.WithExceptionDetails()
+            .Filter.ByExcluding(Matching.FromSource("Microsoft.AspNetCore.StaticFiles"))
+            .Filter.ByExcluding(z => z.MessageTemplate.Text.Contains("specific error"))
+            .WriteTo.Async(writeTo => writeTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticsearchSettings:uri"]))
+            {
+                TypeName = null,
+                AutoRegisterTemplate = true,
+                IndexFormat = "indexlogs",
+                BatchAction = ElasticOpType.Create,
+                CustomFormatter = new EcsTextFormatter(),
+                ModifyConnectionSettings = x => x.BasicAuthentication(configuration["ElasticsearchSettings:username"], configuration["ElasticsearchSettings:password"])
+            }))
+            .WriteTo.Async(writeTo => writeTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}"))
+            .CreateLogger();
+
+        builder.Logging.ClearProviders();
+        builder.Host.UseSerilog(Log.Logger, true);
+
+        return builder;
+    }
+
+    public static WebApplication UseSerilog(this WebApplication app)
+    {
+        app.UseMiddleware<ErrorHandlingMiddleware>();
+        app.UseSerilogRequestLogging(opts =>
+        {
+            opts.EnrichDiagnosticContext = LogEnricherExtensions.EnrichFromRequest;
+        });
+
+        return app;
+    }
+}
