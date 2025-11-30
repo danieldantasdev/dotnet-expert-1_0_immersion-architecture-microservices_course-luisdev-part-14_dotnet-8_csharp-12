12413f6249cb01691a9c30f346bd6db53b3539e9
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-11-30 17:52:39 -0300

    feat: adição do arquivo IEntityBase.cs

---
+++ developments/AwesomeShop.Services.Orders-dia1-observabilidade/src/AwesomeShop.Services.Orders.Domain/Entities/IEntityBase.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomeShop.Services.Orders.Domain.Entities;
+
+public interface IEntityBase
+{
+    Guid Id { get; }
+}
