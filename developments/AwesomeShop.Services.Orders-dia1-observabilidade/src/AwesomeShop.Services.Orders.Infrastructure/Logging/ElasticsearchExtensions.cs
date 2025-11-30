3559d12a40994a6e56b568b459bbb0ffc29844c6
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-11-30 17:52:31 -0300

    feat: adição do arquivo ElasticsearchExtensions.cs

---
+++ developments/AwesomeShop.Services.Orders-dia1-observabilidade/src/AwesomeShop.Services.Orders.Infrastructure/Logging/ElasticsearchExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Elastic.Apm.NetCoreAll;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace AwesomeShop.Services.Orders.Infrastructure.Logging;
+public static class ElasticsearchExtensions
+{
+    public static void UseElasticApm(this IApplicationBuilder app, IConfiguration configuration)
+    {
+        //https://www.elastic.co/guide/en/apm/agent/dotnet/current/configuration-on-asp-net-core.html
+        app.UseAllElasticApm(configuration);
+    }
+}
